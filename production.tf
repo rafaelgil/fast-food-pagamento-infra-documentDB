13019e8fc5df39e9c86febd2a44f0d708b0f9a29
Rafael Carmona Gil <rafaelcarmonagil@gmail.com>
2024-01-26 19:37:45 -0300

    Pipeline

--- production.tf
+++ production.tf
@@ -12,10 +12,10 @@ provider "aws" {
 module "documentDB" {
   source               = "./documentDB"
   environment          = local.environment
-  subnet_ids           = ["subnet-07feb062c5ccf071c", "subnet-0420b3c2c14b17c36"]
-  vpc_id               = "vpc-07a59b130204263c2"
+  subnet_ids           = ["subnet-07cfdc74862fb5a39", "subnet-0d7c3c1a48777ce60"]
+  vpc_id               = "vpc-0fe5292222d495854"
   docdb_instance_class = "db.r5.large"
   vpc_security_group_ids = [
-    "sg-029841ea9f1cbd5cc"
+    "sg-058d9aea3f43b735f"
   ]
 }
